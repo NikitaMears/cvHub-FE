4ed86fefd7182290cba365c97de25ead6bed5226
Melaku Minas <minasmelaku@gmail.com>
2024-03-11 10:10:16 +0300

    after demo

--- src/App.js
+++ src/App.js
@@ -41,8 +41,8 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
+        <Route path="/" exact component={Login} />
+      
         <Route path="/login" exact component={Login} />
 
         <Main>
